fb25095c53f471a8f21115d81f8bc90e668c8435
gabyelizabet <126698477+gabyelizabet@users.noreply.github.com>
2024-08-30 15:26:06 -0300

    Error de tipeo

--- src/images/index.js
+++ src/images/index.js
@@ -1,8 +1,8 @@
-export{default as Photo1} from './photo1.jpeg';
-export{default as Photo2} from './photo2.jpeg';
-export{default as Photo3} from './photo3.webp';
-export{default as Photo4} from './photo4.jpg';
-export{default as Photo5} from './photo5.webp';
-export{default as Photo6} from './photo6.jpg';
+export{default as Photo1} from './Photo1.jpeg';
+export{default as Photo2} from './Photo2.jpeg';
+export{default as Photo3} from './Photo3.webp';
+export{default as Photo4} from './Photo4.jpg';
+export{default as Photo5} from './Photo5.webp';
+export{default as Photo6} from './Photo6.jpg';
 export {default as hambur} from './hamburguesa.webp';
 export {default as Logo} from './Logo.png';
